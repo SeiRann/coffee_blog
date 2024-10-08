0bdfc56185b1f3359c54f3c7551b2e76b749c414
Seiran <bo646ru@gmail.com>
2024-10-08 16:17:03 -0400

    defined models

--- app/api/graphql/models/index.ts
+++ app/api/graphql/models/index.ts
@@ -1,24 +1,104 @@
-// User Model Creation
-const mongoose = require("mongoose")
-const { Schema } = mongoose
+import mongoose, { Schema, Document } from "mongoose"
 
-const userSchema = new Schema({
-	// Define user fields here matching the GraphQL schema
-	first_name: { type: String, required: [true, "All fields are required"] },
-	last_name: {
-		type: String,
-		required: [true, "All fields are required"],
-	},
-	email: {
-		type: String,
-		required: [true, "All fields are required"],
-	},
-	age: {
-		type: String,
-		required: [true, "All fields are required"],
-	},
-	active: Boolean,
+interface GeneralComment {
+	commentAuthor?: string
+	commentText: string
+	commentLikes: number
+}
+
+interface Social {
+	github?: string
+	linkedin?: string
+	discord?: string
+	telegram?: string
+	instagram?: string
+	facebook?: string
+}
+
+interface IReply extends GeneralComment {
+	commentCode: string
+	replies: IReply[]
+}
+
+const replySchema = new Schema<IReply>({
+	commentCode: { type: String, required: true },
+	commentAuthor: String,
+	commentText: { type: String, required: true },
+	commentLikes: { type: Number, default: 0 },
+	replies: [{ type: Schema.Types.ObjectId, ref: "Reply" }],
 })
 
-export default mongoose.models.UserModel ||
-	mongoose.model("UserModel", userSchema)
+interface IPostComment extends GeneralComment {
+	postCode: string
+	replies: IReply[]
+}
+
+const commentPostSchema = new Schema<IPostComment>({
+	postCode: { type: String, required: true },
+	commentAuthor: String,
+	commentText: { type: String, required: true },
+	commentLikes: { type: Number, default: 0 },
+	replies: [{ type: Schema.Types.ObjectId, ref: "Reply" }],
+})
+
+interface IPost extends Document {
+	postCode: string
+	title: string
+	text: string
+	authorUsername?: string
+	category: string
+	datePosted: Date
+	comments: IPostComment[]
+	likes: number
+	views: number
+	images?: string[]
+	thumbnail?: string
+}
+
+const postSchema = new Schema<IPost>({
+	postCode: { type: String, required: true },
+	title: { type: String, required: true },
+	text: { type: String, required: true },
+	authorUsername: String,
+	category: { type: String, required: true },
+	datePosted: { type: Date, required: true },
+	comments: [commentPostSchema],
+	likes: { type: Number, default: 0 },
+	views: { type: Number, default: 0 },
+	images: [{ type: String }],
+	thumbnail: String,
+})
+
+interface IUser extends Document {
+	username: string
+	email: string
+	passwordHash: string
+	interests: string[]
+	posts: IPost[]
+	socials?: Social
+	accountAge: Date
+}
+
+const userSchema = new Schema<IUser>({
+	username: { type: String, required: true },
+	email: { type: String, required: true },
+	passwordHash: { type: String, required: true },
+	interests: [String],
+	posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+	socials: {
+		github: { type: String },
+		linkedin: { type: String },
+		discord: { type: String },
+		telegram: { type: String },
+		instagram: { type: String },
+		facebook: { type: String },
+	},
+	accountAge: { type: Date, required: true },
+})
+
+const User = mongoose.model<IUser>("User", userSchema)
+const Post = mongoose.model<IPost>("Post", postSchema)
+const PostComment = mongoose.model<IPostComment>("Comment", commentPostSchema)
+const Reply = mongoose.model<IReply>("Reply", replySchema)
+
+export { User, Post, PostComment, Reply }
